11f3c94523ec2b6f4c70331410e706825dcbd8f3
Paweł W <pawelwyzykowski@gmail.com>
2018-11-10 11:25:20 +0100

    added handling exception

--- img_api/views.py
+++ img_api/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from rest_framework.response import Response
 from rest_framework import status
 from .models import ScreenShots
+from django.utils.datastructures import MultiValueDictKeyError
 from datetime import datetime
 from django.core.files.images import ImageFile
 # Create your views here.
@@ -11,14 +12,16 @@ from django.core.files.images import ImageFile
 class ImagesApiView(APIView):
     def post(self, request, format=None):
 
-        ScreenShots.objects.create(img=request.FILES['myfile'])
-        my_file = ScreenShots.objects.get(id=1)
-        print(my_file.filename())
-        return Response('Wszystko', status=status.HTTP_200_OK)
+        try:
+            ScreenShots.objects.create(img=request.FILES['screen_shot'])
+            return Response('Wszystko', status=status.HTTP_200_OK)
+        except MultiValueDictKeyError:
+            return Response("There's no file attached!",
+                            status=status.HTTP_406_NOT_ACCEPTABLE)
 
 
 class ListImages(View):
     def get(self, request):
-        ctx = {}
-        images = ScreenShots.objects.all()
+        screen_shots = ScreenShots.objects.all()
+        ctx = {'screen_shots': screen_shots}
         return render(request, 'img_api/index.html', ctx)
